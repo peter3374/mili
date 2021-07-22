17522d30206e443eb32118f3b78ecf72aa5641fd
peter3374 <peter4533@mail.ru>
2021-07-23 01:43:43 +0300

    upd3

--- lib/controller.dart
+++ lib/controller.dart
@@ -5,21 +5,20 @@ import 'package:flashlight/flashlight.dart';
 
 class MyContoller extends GetxController {
   List colorList = [Colors.white, Colors.black];
+
   var isDark = true.obs;
-  var rotation = 0.0.obs;
+
+/*
+Use this variable only for animation;
+*/
+  // var rotation = 0.0.obs;
+
   final BannerAd myBanner = BannerAd(
     adUnitId: 'ca-app-pub-2661805624872444/1020514305',
     size: AdSize.banner,
     request: AdRequest(),
     listener: BannerAdListener(),
   );
-
-  @override
-  void onInit() {
-    super.onInit();
-    myBanner.load();
-    // runRotation();
-  }
 
   void TorchOn() async {
     isDark.value = false;
@@ -31,7 +30,23 @@ class MyContoller extends GetxController {
     Flashlight.lightOff();
   }
 
-// rotate icon
+  @override
+  void onInit() {
+    super.onInit();
+    myBanner.load();
+    // runRotation();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+    myBanner.dispose();
+  }
+
+/*
+Lune rotation animation
+*/
+
   // Future runRotation() async {
   //   for (int i = 0; i < 100000000000000; i++) {
   //     await Future.delayed(Duration(milliseconds: 100));
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-
-import 'package:minimal_light/controller.dart';
+import 'package:mili/controller.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,9 +14,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]); // hide status bar
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
-        // hide debug word
         debugShowCheckedModeBanner: false,
         title: 'Mili',
         theme: ThemeData(
@@ -27,14 +25,14 @@ class MyApp extends StatelessWidget {
           body: SafeArea(
             child: HomePage(),
           ),
-          extendBody: true,
         ));
   }
 }
 
-MyContoller _controller = Get.put(MyContoller());
-
 class HomePage extends StatelessWidget {
+  // getx controller;
+  MyContoller _controller = Get.put(MyContoller());
+
   @override
   Widget build(BuildContext context) {
     return Obx(
@@ -51,6 +49,7 @@ class HomePage extends StatelessWidget {
                 top: 0,
                 left: MediaQuery.of(context).size.width * 0.115,
               ),
+              // ad Banner is here
               child: Container(
                 height: 50,
                 width: 320,
@@ -63,33 +62,30 @@ class HomePage extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Transform.rotate(
-                      angle: 0,
-                      child: InkWell(
-                        onTap: () => _controller.TorchOn(),
-                        child: Icon(
-                          Icons.brightness_1_sharp,
-                          color: _controller.isDark.value
-                              ? _controller.colorList[0]
-                              : _controller.colorList[1],
-                          size: 100,
-                        ),
-                      )),
+                  // Sun icon
+                  IconButton(
+                    icon: Icon(
+                      Icons.brightness_1_sharp,
+                      size: 100,
+                    ),
+                    color: _controller.isDark.value
+                        ? _controller.colorList[0]
+                        : _controller.colorList[1],
+                    onPressed: () => _controller.TorchOn(),
+                  ),
                   SizedBox(
                     height: 50,
                   ),
-                  Transform.rotate(
-                    angle: _controller.rotation.value,
-                    child: InkWell(
-                      onTap: () => _controller.TorchOff(),
-                      child: Icon(
-                        Icons.brightness_2_sharp,
-                        color: _controller.isDark.value
-                            ? _controller.colorList[0]
-                            : _controller.colorList[1],
-                        size: 100,
-                      ),
+                  // Lune icon
+                  IconButton(
+                    color: _controller.isDark.value
+                        ? _controller.colorList[0]
+                        : _controller.colorList[1],
+                    icon: Icon(
+                      Icons.brightness_2_sharp,
+                      size: 100,
                     ),
+                    onPressed: () => _controller.TorchOff(),
                   ),
                 ],
               ),
